718ca4c4cdc38cd2eb3c5d292e73459fc883407c
serkech@gmail.com <xcode@009>
2021-03-05 12:32:55 -0500

    Individual block on the chain

--- index.ts
+++ index.ts
@@ -12,3 +12,24 @@ class Transaction {
         return JSON.stringify(this);
     }
 }
+
+
+// Individual block on the chain
+class Block {
+
+    public nonce = Math.round(Math.random() * 999999999);
+
+    constructor(
+        public prevHash: string,
+        public transaction: Transaction,
+        public ts = Date.now()
+    ) {}
+
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex');
+    }
+}
+
